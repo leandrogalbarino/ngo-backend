49c93dfc46e4d2b3ccdf4a9ebaa2ce950adb8ebf
Leandro Galbarino <leandro.galbarino@pitang.com>
2026-09-22 20:23:15 -0300

    git commit -m "feat: view de documentos retorna os documentos com base no tipo documento"

--- backend/despesas/views/documentos.py
+++ backend/despesas/views/documentos.py
@@ -1,6 +1,7 @@
 from django.db.models import Model
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
 from despesas import serializers
 from despesas.filters import TipoDocumentoFilterSet
@@ -21,8 +22,7 @@ class TipoDocumentoViewSet(AtivoListDefaultMixin, viewsets.ModelViewSet, ):
 class ValorDocumentoViewSet(viewsets.ModelViewSet):
     queryset = ValorDocumento.objects.all()
     serializer_class = ValorDocumentoSerializer
-    http_method_names = ['get', 'patch', 'post', 'delete']
-
+    http_method_names = ['get']
 
     def get_queryset(self):
         id_transacao = self.kwargs.get('transacao_pk')
@@ -30,14 +30,15 @@ class ValorDocumentoViewSet(viewsets.ModelViewSet):
             self.transacao = Transacao.objects.get(pk=id_transacao)
         except Transacao.DoesNotExist:
             raise NotFound(detail='Transação não encontrada')
-
         return ValorDocumento.objects.filter(versao_transacao=self.transacao.versao_transacao)
 
-    # def retrieve(self, request, *args, **kwargs):
-    #     super().get_queryset()
-    #     id_tipo_documento = kwargs.get('pk')
-    #     tipo_documento = ValorDocumento.objects.filter(tipo_documento=id_tipo_documento)
-    #     if not tipo_documento:
-    #         raise NotFound(detail='Tipo de documento não encontrado para essa transação')
-    #     return Response(tipo_documento)
+    def retrieve(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        id_tipo_documento = kwargs.get('pk')
 
+        try:
+            valor_documento = queryset.get(tipo_documento=id_tipo_documento)
+        except ValorDocumento.DoesNotExist:
+            raise NotFound(detail='Tipo de documento não encontrado para essa transação')
+        serializer = self.get_serializer(valor_documento)
+        return Response(serializer.data)
